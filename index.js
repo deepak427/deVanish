1ddcc8062f89f0264909e06a1a3475e7091f54c6
shubham joshi <shubhamjoshi1902@gmail.com>
2024-10-18 22:35:20 +0530

    added incoming call endpoint

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-import Fastify from 'fastify';
-import dotenv from 'dotenv';
-import fastifyFormBody from '@fastify/formbody';
-import fastifyWs from '@fastify/websocket';
-
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import fastifyFormBody from "@fastify/formbody";
+import fastifyWs from "@fastify/websocket";
+import callRoutes from "./routes/callRoutes.js";
 
 dotenv.config();
 
 const { OPENAI_API_KEY } = process.env;
 if (!OPENAI_API_KEY) {
-    console.error('Missing OpenAI API key. Please set it in the .env file.');
-    process.exit(1);
+  console.error("Missing OpenAI API key. Please set it in the .env file.");
+  process.exit(1);
 }
 
 const fastify = Fastify();
 fastify.register(fastifyFormBody);
 fastify.register(fastifyWs);
 
-
+fastify.register(callRoutes);
 
 const PORT = process.env.PORT || 5050;
 fastify.listen({ port: PORT }, (err) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log(`Server running on port ${PORT}`);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server running on port ${PORT}`);
 });
 
-fastify.get('/', async (request, reply) => {
-    reply.send({ message: 'Twilio Media Stream Server is running!' });
+fastify.get("/", async (request, reply) => {
+  reply.send({ message: "Twilio Media Stream Server is running!" });
 });
