7a89d1e7e6d068a70409db5647da24387d2a3ef4
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-10-18 20:32:05 +0530

    basic server setup

---
+++ index.js
@@ -0,0 +1,32 @@
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+import fastifyFormBody from '@fastify/formbody';
+import fastifyWs from '@fastify/websocket';
+
+
+dotenv.config();
+
+const { OPENAI_API_KEY } = process.env;
+if (!OPENAI_API_KEY) {
+    console.error('Missing OpenAI API key. Please set it in the .env file.');
+    process.exit(1);
+}
+
+const fastify = Fastify();
+fastify.register(fastifyFormBody);
+fastify.register(fastifyWs);
+
+
+
+const PORT = process.env.PORT || 5050;
+fastify.listen({ port: PORT }, (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server running on port ${PORT}`);
+});
+
+fastify.get('/', async (request, reply) => {
+    reply.send({ message: 'Twilio Media Stream Server is running!' });
+});
