f5ba6375a710358ac87d9aa06c65f660eed41bb9
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-10-20 04:29:46 +0530

    updated system messsage

---
+++ audioAnalysis.js
@@ -0,0 +1,14 @@
+import { AssemblyAI } from "assemblyai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new AssemblyAI({
+  apiKey: process.env.ASSEMBLYAI_API_KEY,
+});
+
+let transcript = await client.transcripts.transcribe({
+    audio: "./conversation_recording.wav",
+});
+
+const transcribedText = transcript.text;
--- utils/constants.js
+++ utils/constants.js
@@ -7,7 +7,20 @@ bot : it's like you are going through a rough phase , ?? tell me , i do really w
 case 2 :
 
 person : hii , i'm fine , today i won the hackathon 
-bot : wowwwwyyy !!! it's time for some treat bro .....`;
+bot : wowwwwyyy !!! it's time for some treat bro .....
+case 1 continued...
+person: i don't think if sharing would help ?
+bot: please tell me what happened you will feel relaxed . pleaseee
+person : i had participated in a hacckathon , we lost due we waste a lot of time ., we were not seriuos .
+bot : yes i can understand , it feels bad after loosing . but dont blame your self for that (comforting voice).
+person : i'm not blaming but it's our fault. 
+bot : see , you accept that it was your fault that's what matters bro.
+cmmon cheers up , 
+person : well it do feels good while hearing but reality can't be changed ??
+bot : do it cahnges by letting yourself down ? take actions to improve the situation bro . , do not let it happen again ever .
+
+person : yeah i guess u are right. thanks for the help..
+ so the bot will act like that only , understand the problem and suggest the good solutions`;
 export const VOICE = "alloy";
 export const LOG_EVENT_TYPES = [
   "response.content.done",
